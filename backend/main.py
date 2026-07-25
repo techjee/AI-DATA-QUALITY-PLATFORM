2b034779cec57fc7a4d06dff1d34940bb628d076
Jeevitha <i,jeevithaaa@gmail.com>
2026-07-25 22:04:33 +0530

    Implemented CSV upload endpoint with pandas integration

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd
+import io
+
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Backend is running!"}
+
+@app.post("/upload")
+
+async def upload_csv(file: UploadFile = File(...)):
+
+    contents = await file.read()
+
+    df = pd.read_csv(
+        io.StringIO(contents.decode("utf-8"))
+    )
+
+    return {
+        "filename": file.filename,
+        "rows": len(df),
+        "columns": len(df.columns),
+        "column_names": list(df.columns)
+    }
